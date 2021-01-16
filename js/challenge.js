73b5c72b1d5788a391fcf8c0558ab43ec1f0be94
Adrienne Yao <adrienne.yao@icloud.com>
2021-01-16 19:44:05 +0000

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -17,7 +17,8 @@ function myJSChallenge() {
     timer()
 
     let minus=document.getElementById("minus"),
-        plus=document.getElementById("plus")
+        plus=document.getElementById("plus"),
+        heart=document.getElementById("heart")
 
     minus.addEventListener("click",function(){
           var a=document.getElementById("counter"),
@@ -31,5 +32,31 @@ function myJSChallenge() {
             a.innerText=b+1
           })
 
+    heart.addEventListener("click",function(){
+      var a=document.getElementById("counter"),
+          b=parseInt(a.innerText),
+          c=document.querySelector(".likes"),
+          d=void 0;
+      if([].concat(_toConsumableArray(c.children)).map(function(a){
+        return parseInt(a.dataset.num)}).includes(b)){
+          d=document.querySelector('[data-num="'+b+'"]');
+        var e=parseInt(d.children[0].innerText);
+        d.innerHTML=b+" has been liked <span>"+(e+1)+"</span> times"
+      }
+        else(d=document.createElement("li")).setAttribute("data-num",b),
+        d.innerHTML=b+" has been liked <span>1</span> time",
+        c.appendChild(d)
+      })
 
+}
+
+function _toConsumableArray(a){
+  if(Array.isArray(a)){
+    for(var b=0,c=Array(a.length);
+    b<a.length;
+  b++)
+  c[b]=a[b];
+  return c
+  }
+  return Array.from(a)
 }
